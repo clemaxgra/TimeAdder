2c12a1674c91df63c87367df8ea1e385db15af94
clemaxgra <clempowen@hotmail.com>
2015-04-28 02:22:24 +0800

    Updated Files
    Added function to add 20 minutes and added 1 extra line to be printed in
    'randomFunction' function

--- R/TimeAdder.R
+++ R/TimeAdder.R
@@ -4,10 +4,18 @@ randomFunction <- function()
   print(paste0("Current time is ", time))
   time2 <- add10Minutes(time)
   print(paste0("That time with 10 minutes added is ", time2))
+  time3 <- add20Minutes(time)
+  print(paste0("That time with 20 minutes added is ", time3))
 }
 
 add10Minutes <- function(time)
 {
   result <- time + 10 * 60
+  return (result)
+}
+
+add20Minutes <- function(time)
+{
+  result <- time + 20 * 60
   return (result)
 }
